21df8fcf8ffd50a3a478b459b0d06cfa1470df1c
dcuddhy <dcuddhy@gmail.com>
2020-10-07 22:00:17 -0600

    correcting file caps, step 2

--- src/views/Index.js
+++ src/views/Index.js
@@ -2,7 +2,7 @@ import React, {useContext, useState} from 'react';
 import { Dropdown } from '../components/Dropdown';
 import { Filter } from '../components/Filter';
 import { generateList } from '../components/Helpers';
-import { Table } from '../components/TableTable';
+import { Table } from '../components/Table';
 import { FilterContext } from '../contexts/filterContext'
 
 export const Index = (data) => {
